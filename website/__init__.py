f7d41a0016ae87220d5ba3beb4b56bab665f881f
KiseraTimon <timonmarick@gmail.com>
2025-05-30 15:29:40 +0300

    #4 - Registering Blueprints

--- website/__init__.py
+++ website/__init__.py
@@ -9,4 +9,15 @@ def create_app():
     # Configuring secret key
     app.config['SECRET_KEY'] = 'testapp'
 
+    # Importing blueprints
+    from .auth import auth
+    from .processes import processes
+    from .views import views
+    # from .dashboard import dashboard
+
+    # Registering blueprints
+    app.register_blueprint(views, url_prefix='/')
+    app.register_blueprint(auth, url_prefix='/')
+    app.register_blueprint(processes, url_prefix='/')
+
     return app
