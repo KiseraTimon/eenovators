5a6950e41faa39d5fa7bf68d396c4e848f117d40
KiseraTimon <timonmarick@gmail.com>
2025-05-30 20:43:13 +0300

    #33 - Extracting Meter Registers

--- website/processes.py
+++ website/processes.py
@@ -52,7 +52,92 @@ def newsletter():
 
     return redirect(url_for('views.homepage'))
 
+# Extracting Registers Data
+@processes.route('/registers')
+def registers():
 
-@processes.route('/api')
-def api():
-    return '<h1>API endpoint is accessible</h1>'
+    try:
+        # Modules
+        import argparse
+        import sys
+        from datetime import datetime, timezone
+
+        from egauge.examples import test_common
+        from egauge.webapi.device import PhysicalQuantity, Register, UnitSystem
+
+        # Parsers
+        parser = argparse.ArgumentParser(
+            description="Demonstrate the use of class egauge.webapi.device.Register."
+        )
+        parser.add_argument(
+            "--imperial",
+            action="store_true",
+            help="Output units in imperial units rather than metric units.",
+        )
+        args = parser.parse_args()
+
+        # Initializing Register Value Units
+        if args.imperial:
+            PhysicalQuantity.set_unit_system(UnitSystem.IMPERIAL)
+
+        # Capturing Response
+        ret = Register(test_common.dev, {"rate": "", "time": "now,som"})
+
+        ts = ret.ts()
+
+        if ts is None:
+            message("Failed to get register data.", file=sys.stderr)
+            sys.exit(1)
+
+        # Initial Data timestamp
+        init_dt = datetime.fromtimestamp(round(ts), tz=timezone.utc)
+        init_time_str = init_dt.strftime("%Y-%m-%d %H:%M:%S %Z")
+
+        # Initial Rates Dictionary
+        initial_rates = {}
+
+        # Initial Register Rates
+        for regname in ret.regs:
+            # Instantananeous Rate
+            pq = ret.pq_rate(regname)
+            initial_rates[regname] = {
+                "value": round(pq.value, 3),
+                "unit": pq.unit
+            }
+
+
+        # Calculating deltas (Change in values)
+        delta = ret[0] - ret[1]
+
+        # Delta Data Timestamp
+        delta_dt = datetime.fromtimestamp(round(ret[1].ts), tz=timezone.utc)
+        delta_time_str = delta_dt.strftime("%Y-%m-%d %H:%M:%S %Z")
+
+        # Delta Register Dictionary
+        delta_rates = {}
+
+        # Value changes since start of month
+        for regname in delta.regs:
+            # Accumulated value
+            accu = delta.pq_accu(regname)
+            avg = delta.pq_avg (regname)
+
+            delta_rates[regname] = {
+                "accumulated": round(accu.value, 3),
+                "average": round(avg.value, 3),
+                "unit": accu.unit
+            }
+
+        return render_template(
+            'report/report.html',
+            init_time = init_time_str,
+            delta_time = delta_time_str,
+            initials = initial_rates,
+            deltas = delta_rates
+            )
+
+    except Exception as e:
+        errhandler (e, "processes")
+        return redirect(url_for('views.homepage'))
+
+    return render_template('report/report.html')
